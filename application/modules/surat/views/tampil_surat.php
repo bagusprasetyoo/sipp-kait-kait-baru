e7bb55dd8dc247eda0da1516722a0949bc784658
1114 BAGUS PRASETYO <bagus.prasetyo@mhs.politala.ac.id>
2022-11-02 12:19:30 +0800

    update

--- application/modules/surat/views/tampil_surat.php
+++ application/modules/surat/views/tampil_surat.php
@@ -1,123 +1,122 @@
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
 
-            <!-- Main content -->
-            <section class="content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-
-                                <!-- /.content-header -->
-                                <div class="content-header">
-                                    <div class="container-fluid">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <a href="<?= base url('penduduk/add_pend'); ?>" class="btn btn-success">+ Buat
-                                                    Surat </a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <!-- /.card-header -->
-                                    <div class="card-body">
-                                        <table id="example1" class="table table-bordered table-striped table-sm">
-                                            <thead>
-                                                <tr>
-                                                    <th>No</th>
-                                                    <th>No Surat</th>
-                                                    <th>Jenis Surat</th>
-                                                    <th>NIK</th>
-                                                    <th>Dikirim Oleh</th>
-                                                    <th>Tanggal Surat</th>
-                                                    <th>Status</th>
-                                                    <th>Aksi</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <?php $no = 1;
-                                                    foreach ($row->result() as $key => $data) { ?>
-                                                        <td><?= $no++; ?></td>
-                                                        <td><?= $data->nosurat; ?></td>
-                                                        <td><?= $data->jenis_surat; ?></td>
-                                                        <td><?= $data->nik; ?></td>
-                                                        <td><?= $data->dibuat_oleh; ?></td>
-                                                        <td><?= $data->tanggal_surat; ?></td>
-                                                        <td><?= $data->status; ?></td>
-                                                        <td><?= $data->aksi; ?></td>
-                                                        <td>
-                                                            <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modalpend<?= $data->nik; ?>">
-                                                                <i class="fas fa-eye"></i></button>
-                                                            <a href="<?= base_url(); ?>" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a>
-                                                            <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
-                                                        </td>
-                                                </tr>
-                                            <?php } ?>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    <!-- /.card-body -->
+                    <!-- /.content-header -->
+                    <div class="content-header">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-md-3">
+                                    <a href="<?= base_url('penduduk/add_pend'); ?>" class="btn btn-success"> + Buat
+                                        Surat </a>
                                 </div>
-                                <!-- /.card -->
                             </div>
-                            <!-- /.col -->
                         </div>
-                        <!-- /.row -->
+
+                        <!-- /.card-header -->
+                        <div class="card-body">
+                            <table id="example1" class="table table-bordered table-striped table-sm">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>No Surat</th>
+                                        <th>Jenis Surat</th>
+                                        <th>NIK</th>
+                                        <th>Dikirim Oleh</th>
+                                        <th>Tanggal Surat</th>
+                                        <th>Status</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <?php $no = 1;
+                                        foreach ($row->result() as $key => $data) { ?>
+                                            <td><?= $no++; ?></td>
+                                            <td><?= $data->nosurat; ?></td>
+                                            <td><?= $data->jenis_surat; ?></td>
+                                            <td><?= $data->nik; ?></td>
+                                            <td><?= $data->dibuat_oleh; ?></td>
+                                            <td><?= $data->tanggal_surat; ?></td>
+                                            <td><?= $data->status; ?></td>
+                                            <td><?= $data->aksi; ?></td>
+                                            <td>
+                                                <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#modalpend<?= $data->nik; ?>">
+                                                    <i class="fas fa-eye"></i></button>
+                                                <a href="<?= base_url(); ?>" class="btn btn-warning btn-sm"><i class="fas fa-edit"></i></a>
+                                                <a href="#" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></a>
+                                            </td>
+                                    </tr>
+                                <?php } ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
                     </div>
-                    <!-- /.container-fluid -->
-            </section>
-            <!-- /.content -->
-
-        </div>
-        <!-- /.content-wrapper -->
-        <footer class="main-footer text-center bg-dark">
-            <strong>Copyright &copy; 2022 </strong>
-            All rights reserved.
-            <div class="float-right d-none d-sm-inline-block">
-                <b>Version</b> B4 ALTE 3
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
             </div>
-        </footer>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
 
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Control sidebar content goes here -->
-        </aside>
-        <!-- /.control-sidebar -->
+</div>
+<!-- /.content-wrapper -->
+<footer class="main-footer text-center bg-dark">
+    <strong>Copyright &copy; 2022 </strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+        <b>Version</b> B4 ALTE 3
     </div>
-    <!-- ./wrapper -->
+</footer>
 
-    <!-- REQUIRED SCRIPTS -->
-    <!-- jQuery -->
-    <script src="/plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="/dist/js/adminlte.min.js"></script>
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
 
-    <!-- Tabel Script -->
-    <!-- DataTables -->
-    <script src="/plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<!-- REQUIRED SCRIPTS -->
+<!-- jQuery -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/dist/js/adminlte.min.js"></script>
 
-    <!-- table page script -->
-    <script>
-        $(function () {
-            $("#example1").DataTable({
-                "responsive": true,
-                "autoWidth": false,
-            });
-            $('#example2').DataTable({
-                "paging": true,
-                "lengthChange": false,
-                "searching": false,
-                "ordering": true,
-                "info": true,
-                "autoWidth": false,
-                "responsive": true,
-            });
+<!-- Tabel Script -->
+<!-- DataTables -->
+<script src="/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+
+<!-- table page script -->
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            "responsive": true,
+            "autoWidth": false,
         });
-    </script>
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+        });
+    });
+</script>
 </body>
 
 </html>
