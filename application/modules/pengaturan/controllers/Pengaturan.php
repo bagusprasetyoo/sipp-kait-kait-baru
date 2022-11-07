d9670f2730c1a482d1b301f5b09ede177dd61b8e
Muhammad Mauladi <muhammad.mauladi@mhs.politala.ac.id>
2022-11-07 20:34:59 +0800

    pengaturan profile

--- application/modules/pengaturan/controllers/Pengaturan.php
+++ application/modules/pengaturan/controllers/Pengaturan.php
@@ -15,12 +15,56 @@ class Pengaturan extends CI_Controller
     {
         $data['title'] = 'Pengaturan Profile';
         $data['user'] = $this->db->get_where('tb_pengguna', ['nik' => $this->session->userdata('nik')])->row_array();
-        $this->load->view('template/user_header', $data);
-        $this->load->view('template/user_sidebar', $data);
-        $this->load->view('pengaturan/edit_profile', $data);
-        $this->load->view('template/user_footer');
+
+        $this->form_validation->set_rules('email',  'Email', 'required|trim', [
+            'required' => 'Email harus diisi !'
+        ]);
+        $this->form_validation->set_rules('no_hp',  'No HP', 'required|trim', [
+            'required' => 'No HP harus diisi !'
+        ]);
+
+        if ($this->form_validation->run() == false) {
+            $this->load->view('template/user_header', $data);
+            $this->load->view('template/user_sidebar', $data);
+            $this->load->view('pengaturan/edit_profile', $data);
+            $this->load->view('template/user_footer');
+        } else {
+            $email = $this->input->post('email');
+            $no_hp = $this->input->post('no_hp');
+            $nik = $this->input->post('nik');
+
+            //cek gambar
+            $upload_image = $_FILES['image']['nama'];
+
+            if ($upload_image) {
+                $config['upload_path'] = './assets/img/profile/';
+                $config['allowed_types'] = 'gif|jpg|png';
+                $config['max_size']     = '2048';
+
+                $this->load->library('upload', $config);
+
+                if (!$this->upload->do_upload('image')) {
+                    $old_image = $data['user']['image'];
+                    if ($old_image != 'default.jpg') {
+                        unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                    }
+
+                    $new_image = $this->upload->data('file_name');
+                    $this->db->set('image', $new_image);
+                } else {
+                    echo $this->upload->dispay_errors();
+                }
+            }
+
+            $this->db->set('email', $email);
+            $this->db->set('no_hp', $no_hp);
+            $this->db->where('nik', $nik);
+            $this->db->update('tb_pengguna');
+
+            $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-dismissible fade show">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <small><i class="icon fas fa-check"></i> Selamat Profile Berhasil diUbah.</small></div>');
+            redirect('pengaturan/edit_profile');
+        }
     }
-
-    
-
 }
--- application/modules/pengaturan/views/edit_profile.php
+++ application/modules/pengaturan/views/edit_profile.php
@@ -16,39 +16,39 @@
                 
                 <?= $this->session->flashdata('pesan'); ?>
                 <form class="form-horizontal">
-                <form class="form-horizontal" action="<?= base_url('pengaturan/edit_profile') ?>" method="post">
+                <?php echo form_open_multipart('pengaturan/edit_profile'); ?>
                     <div class="form-group row">
                         <label for="nama" class="col-sm-2 col-form-label">Nama Lengkap</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="nama" placeholder="nama" value="<?= $this->fungsi->user_login()->nama; ?>" readonly>
+                            <input type="text" class="form-control" id="name" name="nama" placeholder="nama" value="<?= $this->fungsi->user_login()->nama; ?>" readonly>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="nik" class="col-sm-2 col-form-label">NIK</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="nik" placeholder="NIK" value="<?= $user['nik']; ?>" readonly>
+                            <input type="text" class="form-control" id="nik" name="nik" placeholder="NIK" value="<?= $user['nik']; ?>" readonly>
                             <?= form_error('nik', '<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="email" class="col-sm-2 col-form-label">Email</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="email" placeholder="Email" value="<?= $user['email']; ?>">
+                            <input type="text" class="form-control" id="email" name="email" placeholder="Email" value="<?= $user['email']; ?>">
                             <?= form_error('email', '<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="nohp" class="col-sm-2 col-form-label">No Handphone</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="nohp" placeholder="No Handphone" value="<?= $user['no_hp']; ?>">
-                            <?= form_error('nohp', '<small class="text-danger pl-3">','</small>'); ?>
+                            <input type="text" class="form-control" id="no_hp" name="no_hp" placeholder="No Handphone" value="<?= $user['no_hp']; ?>">
+                            <?= form_error('no_hp', '<small class="text-danger pl-3">','</small>'); ?>
                         </div>
                     </div>
                     <div class="form-group row">
                         <label for="file" class="col-sm-2 col-form-label">Gambar Profil</label>
                         <div class="col-sm-10">
                             <div class="custom-file">
-                                <input type="file" class="custom-file-input" name="image" value="<?= $user['image']; ?>">
+                                <input type="file" class="custom-file-input" id="image" name="image" value="<?= $user['image']; ?>">
                                 <label class=" custom-file-label" for="image">Ubah Gambar</label>
                             </div>
                         </div>
